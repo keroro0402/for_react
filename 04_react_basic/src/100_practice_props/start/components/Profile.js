bdc06c5b7f42cf15a41ac5b9a50115fbe47619b4
keroro0402 <otsuyu0402@yahoo.co.jp>
2024-02-03 23:16:30 +0900

    Props練習

--- 04_react_basic/src/100_practice_props/start/components/Profile.js
+++ 04_react_basic/src/100_practice_props/start/components/Profile.js
@@ -1,12 +1,15 @@
-import "./Profile.css";
+import './Profile.css';
 
 const Profile = (props) => {
+  console.log(props);
+  let { name = 'John Doe', age = '??', country = 'Japan' } = props;
   return (
-    <div className="profile">
-
+    <div className='profile'>
+      <p>Name : {name}</p>
+      <p>Age : {age}</p>
+      <p>From :{country} </p>
     </div>
   );
 };
-
 
 export default Profile;
