4e1d83871ce2806d3a47c2b8975ea44402dfe72d
keroro0402 <otsuyu0402@yahoo.co.jp>
2024-02-04 17:11:32 +0900

    ステート

--- 05_state_and_event/src/020_useState/start/Example.js
+++ 05_state_and_event/src/020_useState/start/Example.js
@@ -3,8 +3,42 @@ import { useState } from 'react';
 const Example = () => {
   let [val, setVal] = useState('');
 
+  let [val2, setVal2] = useState('');
+
+  let [val3, setVal3] = useState(0);
+
+  let mkinp = function () {
+    return {
+      id: Math.floor(Math.random() * 1e4),
+      val: <input type='text' />,
+    };
+  };
+
+  let arrayInit = [mkinp(), mkinp(), mkinp()];
+
+  let [array, setArray] = useState(arrayInit);
   return (
     <>
+      <button
+        onClick={() => {
+          setArray([mkinp(), ...array]);
+        }}
+      >
+        追加
+      </button>
+      <br></br>
+      {array.map((item) => {
+        return <p key={item.id}>{item.val}</p>;
+      })}
+      <hr></hr>
+      <input
+        type='button'
+        onClick={() => {
+          setVal3((pre) => (pre += 1));
+        }}
+      />
+      カウント
+      <p>{val3}</p>
       <input
         type='text'
         onChange={(e) => {
@@ -12,6 +46,15 @@ const Example = () => {
         }}
       />
       ={val}
+      <br></br>
+      <br></br>
+      <input
+        type='text'
+        onChange={(e) => {
+          setVal2(e.target.value);
+        }}
+      />
+      ={val2}
     </>
   );
 };
