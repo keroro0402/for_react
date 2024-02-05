647fc0a2039de67644a0f3734f2d8d14470d7648
keroro0402 <otsuyu0402@yahoo.co.jp>
2024-02-05 23:22:23 +0900

    useStateの練習

--- 05_state_and_event/src/040_multiple_state/start/Example.js
+++ 05_state_and_event/src/040_multiple_state/start/Example.js
@@ -1,5 +1,43 @@
+import { useState } from 'react';
+
 const Example = () => {
-  
+  let [count1, setCount1] = useState(0);
+  let [count2, setCount2] = useState(10);
+  let [count3, setCount3] = useState(100);
+  return (
+    <>
+      <button
+        onClick={() => {
+          setCount1((pre) => {
+            return (pre += 1);
+          });
+        }}
+      >
+        ボタンA
+      </button>
+      <p>ボタンAを{count1}回押しました！！</p>
+      <button
+        onClick={() => {
+          setCount2((pre) => {
+            return (pre += 1);
+          });
+        }}
+      >
+        ボタンB
+      </button>
+      <p>ボタンBを{count2}回押しました！！</p>
+      <button
+        onClick={() => {
+          setCount3((pre) => {
+            return (pre += 1);
+          });
+        }}
+      >
+        ボタンC
+      </button>
+      <p>ボタンCを{count3}回押しました！！</p>
+    </>
+  );
 };
 
 export default Example;
