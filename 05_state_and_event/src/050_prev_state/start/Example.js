4f9a460ec05e147dc6b523b242132fbb8210da65
keroro0402 <otsuyu0402@yahoo.co.jp>
2024-02-05 23:27:04 +0900

    useStateは非同期で更新することを忘れてはいけない！！

--- 05_state_and_event/src/050_prev_state/start/Example.js
+++ 05_state_and_event/src/050_prev_state/start/Example.js
@@ -1,7 +1,31 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 const Example = () => {
-  
+  let [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p>現在のカウント数：{count}回</p>
+      <button
+        onClick={() => {
+          setCount((pre) => {
+            return pre + 1;
+          });
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          setCount((pre) => {
+            return pre - 1;
+          });
+        }}
+      >
+        -
+      </button>
+    </>
+  );
 };
 
 export default Example;
