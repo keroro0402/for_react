2955fa563c987e7bf5ed8aa552c66369e1064945
keroro0402 <otsuyu0402@yahoo.co.jp>
2024-02-11 16:38:23 +0900

    オブジェクトのステート練習

--- 05_state_and_event/src/068_practice_obj_state/end/Example.js
+++ 05_state_and_event/src/068_practice_obj_state/end/Example.js
@@ -5,19 +5,21 @@ const Example = () => {
   const [order, setOrder] = useState(orderObj);
   const changeItem = (e) => {
     // POINT オブジェクトを複製して新しいオブジェクトを生成
-    setOrder(order => ({ ...order, item: e.target.value }));
+    setOrder((order) => ({ ...order, item: e.target.value }));
   };
   const countUp = () => {
-    setOrder(order => ({ ...order, count: order.count + 1 }));
+    setOrder((order) => ({ ...order, count: order.count + 1 }));
   };
   const countDown = () => {
-    setOrder(order => ({ ...order, count: order.count - 1 }));
+    setOrder((order) => {
+      return { ...order, count: order.count - 1 };
+    });
   };
   return (
     <>
       <h3>Item:{order.item}</h3>
       <h3>Count:{order.count}</h3>
-      <input type="text" value={order.item} onChange={changeItem} />
+      <input type='text' value={order.item} onChange={changeItem} />
       <button onClick={countUp}>+</button>
       <button onClick={countDown}>-</button>
     </>
