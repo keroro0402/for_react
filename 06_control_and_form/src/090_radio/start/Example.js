cadb7d772d6019af01721a56315941c58b19b27a
keroro0402 <otsuyu0402@yahoo.co.jp>
2024-02-18 15:48:56 +0900

    スタイル

--- 06_control_and_form/src/090_radio/start/Example.js
+++ 06_control_and_form/src/090_radio/start/Example.js
@@ -1,10 +1,44 @@
+import { useState } from 'react';
+
 const Example = () => {
+  let [val, setVal] = useState('APPLE');
   return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
+    <>
+      <input
+        name='item'
+        value='APPLE'
+        type='radio'
+        checked
+        onChange={(e) => {
+          setVal(e.target.value);
+        }}
+      />
+      APPLE
+      <input
+        name='item'
+        value='BANANA'
+        type='radio'
+        onChange={(e) => {
+          setVal(e.target.value);
+        }}
+      />
+      BANANA
+      <input
+        name='item'
+        value='CHERRY'
+        type='radio'
+        onChange={(e) => {
+          setVal(e.target.value);
+        }}
+      />
+      CHERRY
+      <h3>{val}</h3>
+      <p style={{ textAlign: 'center' }}>
+        startフォルダの内容が表示されます。
+        <br />
+        練習用に使ってください！
+      </p>
+    </>
   );
 };
 
