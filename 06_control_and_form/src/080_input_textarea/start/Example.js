963ce918edc6ae93884fc7f7a3ec8cc882dafda2
keroro0402 <otsuyu0402@yahoo.co.jp>
2024-02-18 13:00:28 +0900

    れんちゅう

--- 06_control_and_form/src/080_input_textarea/start/Example.js
+++ 06_control_and_form/src/080_input_textarea/start/Example.js
@@ -1,10 +1,36 @@
+import { useState } from 'react';
+
 const Example = () => {
+  let [val, setVal] = useState('');
   return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
+    <>
+      <p style={{ textAlign: 'center' }}>
+        startフォルダの内容が表示されます。
+        <br />
+        練習用に使ってください！
+        <input
+          value={val}
+          type='text'
+          onChange={(e) => {
+            setVal(e.target.value);
+          }}
+        />
+        <textarea
+          value={val}
+          onChange={(e) => {
+            setVal(e.target.value);
+          }}
+        ></textarea>
+      </p>
+      <p>{val}</p>
+      <button
+        onClick={() => {
+          setVal('');
+        }}
+      >
+        クリア
+      </button>
+    </>
   );
 };
 
