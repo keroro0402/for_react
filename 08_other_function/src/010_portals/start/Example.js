836860e425c0264258da95d7ad7f32b1b559fc20
keroro0402 <otsuyu0402@yahoo.co.jp>
2024-02-25 17:39:37 +0900

    createPortal記述済み

--- 08_other_function/src/010_portals/start/Example.js
+++ 08_other_function/src/010_portals/start/Example.js
@@ -1,1 +1,35 @@
-export default () => {};
+import { createPortal } from 'react-dom';
+import { useState } from 'react';
+import Modal from './components/Modal';
+
+export default () => {
+  let [flg, setFlg] = useState(false);
+
+  let PortalModal = ({ children }) => {
+    let target = document.querySelector('.container.end');
+    return createPortal(children, target);
+  };
+
+  return (
+    <>
+      <button
+        disabled={flg}
+        onClick={() => {
+          setFlg(true);
+        }}
+      >
+        {flg ? 'モーダルを非表示にする' : 'モーダルを表示にする'}
+      </button>
+      <div className='container end'></div>
+      {flg && (
+        <PortalModal>
+          <Modal
+            handleCloseClick={() => {
+              setFlg(false);
+            }}
+          />
+        </PortalModal>
+      )}
+    </>
+  );
+};
