4de1ed758fa9c2680a9a2015793dcfd6874cd7fa
keroro0402 <otsuyu0402@yahoo.co.jp>
2024-02-25 13:53:17 +0900

    スタイル記述

--- 04_react_basic/src/060_styling/start/Example.js
+++ 04_react_basic/src/060_styling/start/Example.js
@@ -1,3 +1,19 @@
-function practice() {}
+import './style.scss';
+
+function practice() {
+  return (
+    <>
+      <h1 className='h1 textCenter'>スタイルの当て方</h1>
+      <section className='main'>
+        <div className='main__item'>
+          <h2 className='main__item--title'>SCSSで書いてる</h2>
+          <p className='main__item--txt'>
+            ReactではClass名を「ClassName」属性として付けるようにする。
+          </p>
+        </div>
+      </section>
+    </>
+  );
+}
 
 export default practice;
