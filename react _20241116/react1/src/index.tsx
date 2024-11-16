d0efe58b6412c9a759b16ba8a52a66a75f852133
keroro0402 <otsuyu0402@yahoo.co.jp>
2024-11-16 15:41:50 +0900

    複製

--- react _20241116/react1/src/index.tsx
+++ react _20241116/react1/src/index.tsx
@@ -1,23 +1,21 @@
-const array: number[] = [1, 2, 3, 4, 5];
-let array2: number[] = array.map((item: number) => item * 4).filter((item) => item > 10);
+const array1: number[] = [1, 2, 3, 4, 5];
 
-console.log(array);
-console.log(array2);
+const array3 = [...array1];
+array1.push(99);
 
-const array3 = [
-  { age: 12, name: '佐々木希' },
-  { age: 32, name: '安倍ひろし' },
-];
+console.log(array1);
+console.log(array3);
 
-const [a, b] = [...array3];
-console.log(a);
-console.log(b);
+interface Obj {
+  name: String;
+  age?: String;
+}
 
-const obj = { age: 12121, name: '佐々木希' };
-const { name, age } = obj;
-console.log(name);
-console.log(age);
+const sasaki: Obj = {
+  name: '佐々木希',
+};
+sasaki.age = '43';
+const adachi: Obj = sasaki;
 
-const obj2 = { age: 12121, name: '佐々木希' };
-const obj3 = { ...obj2, sex: '女' };
-console.log(obj3);
+console.log(sasaki);
+console.log(adachi);
